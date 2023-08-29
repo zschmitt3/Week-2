c73f97bdb37d6443026f45d4b2202de38d0de64c
zschmitt3 <zschmitt3@my.wctc.edu>
2023-08-29 10:01:50 -0500

    Commit

---
+++ wwwroot/js/client.js
@@ -0,0 +1,26 @@
+﻿$(function(){
+    // preload audio
+    var toast = new Audio('media/toast.wav');
+
+    $('.code').on('click', function(e) {
+        document.getElementById('product').innerHTML = $(this).data( "name" );
+        document.getElementById('codeSpan').innerHTML = $(this).data( "code" );
+
+
+        e.preventDefault();
+        // first pause the audio (in case it is still playing)
+        toast.pause();
+        // reset the audio
+        toast.currentTime = 0;
+        // play audio
+        toast.play();
+        $('#toast').toast({ autohide: false }).toast('show');
+        
+    });
+});
+$(document).keyup(function(e) {
+    if (e.key === "Escape") {
+        $('#toast').remove();
+        //Nukes HTML, one time use. It's funny, so I'm keeping it.
+   }
+});
